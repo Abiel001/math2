38f7a4136c09ea77745e0bcd9cd9f4fc6d0b9c3c
Abiel Mainty <maintabi001@warren.k12.in.us>
2024-08-29 08:47:13 -0400

    fix

--- main.py
+++ main.py
@@ -6,9 +6,4 @@ mph = miles / hours
 
 print("How many miles did you run?", miles)
 print("How many minutes did", minutes)
-
-#INSTRUCTION write a print statement
-# that says "Speed in mph:"
-# use the mph variable
-print ("speed in mph: ")
-print ("miles / hours")
+print ("speed in mph: ", mph)
