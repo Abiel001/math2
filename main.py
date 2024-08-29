e1d1420f312373f91f38d20b2d88b5622e9c130a
Abiel Mainty <maintabi001@warren.k12.in.us>
2024-08-29 08:45:39 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+miles = 5
+minutes = 45
+hours = minutes / 60
+
+mph = miles / hours
+
+print("How many miles did you run?", miles)
+print("How many minutes did", minutes)
+
+#INSTRUCTION write a print statement
+# that says "Speed in mph:"
+# use the mph variable
+print ("speed in mph: ")
+print ("miles / hours")
